480de2f00013e41c085b7023cedc6daf4db0bc8e
Lakshu Narasu <ced15i004@iiitdm.ac.in>
2023-02-14 11:29:03 +0530

    Buttons,Chips,Snackbars

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,7 +16,7 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {
   Colors,
   DebugInstructions,
@@ -24,75 +24,20 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import { ButtonComponent, IconButtonComponent, Customchips, SnackBar } from './AppModules/Views/Home';
+import { Divider } from 'react-native-paper'
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+<SafeAreaProvider style={{flex:1,height: '100%',width: '100%'}}>
+    <View style={styles.container}>
+       <Text style={{ color: 'white' }}>Hello </Text>
+        <Divider bold={true} horizontalInset={true} />
+        <ButtonComponent />
+        <Divider />
+        <SnackBar />
+    </View>
+</SafeAreaProvider>
   );
 }
 
@@ -100,6 +45,12 @@ const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 32,
   },
   sectionTitle: {
     fontSize: 24,
--- AppModules/Views/Home.tsx
+++ AppModules/Views/Home.tsx
@@ -1,2 +1,68 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native';
+import { Button, Dialog, Portal, Text, IconButton, MD3Colors,Chip,Snackbar   } from 'react-native-paper'
+export const ButtonComponent = () => {
+    return (
+        <Button
+            icon="camera"
+            mode='outlined'
+            buttonColor='violet'
+            textColor='black'
+            onPress={() => { alert('Button clicked') }}>
+            Press Me
+        </Button>
+    )
+}
+export const IconButtonComponent = () => {
+    return (
+        <IconButton
+            icon="camera"
+            mode='outlined'
+            containerColor='lightgrey'
+            iconColor={MD3Colors.error50}
+            size={46}
+            onPress={() => console.log('Pressed')}
+        />
+    )
+}
+export const Customchips = () =>{
+    return (
+        <Chip
+         icon="information" 
+        onPress={() => console.log('Pressed')}>Example Chip</Chip>
+    )
+}
+export const SnackBar = () => {
+    const [visible, setVisible] = React.useState(false);
+  
+    const onToggleSnackBar = () => setVisible(!visible);
+  
+    const onDismissSnackBar = () => setVisible(false);
+  
+    return (
+      <View style={styles.container}>
+        <Button 
+        buttonColor='violet'
+        textColor='black'
+        onPress={onToggleSnackBar}>{visible ? 'Hide' : 'Show'}</Button>
+        <Snackbar
+          visible={visible}
+          onDismiss={onDismissSnackBar}
+          action={{
+            label: 'Undo',
+            onPress: () => {
+              // Do something
+            },
+          }}>
+          Hey there! I'm a Snackbar.
+        </Snackbar>
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'space-between',
+    },
+  });
